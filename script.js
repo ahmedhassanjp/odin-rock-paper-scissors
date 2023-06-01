4ac2b881a5aff5d3cad1c0bbb74c45bb8d5fdb04
ahmed <ahmed.hassan.jp@gmail.com>
2023-06-01 04:10:43 +0300

    Add comments

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Declare computerChoice function that generates a random choice
 function getComputerChoice() {
+  // Create an array of choices
   const choices = ["Rock", "Paper", "Scissors"];
-  var choice = Math.floor(Math.random() * choices.length);
+  // Create a logic for choosing the random choice from the array
+  var choice = Math.floor(Math.random() * 3);          /* Since [choices.length = 3] */
+  // Return the chosen choice
   return choices[choice];
 }
 
+// Declare playGround function that holds the game logic
 function playGround() {
+  // In the next 3 lines we try to manipulate the prompt value taken from user so that it's not case-sensetive by:
+  // 1- Target the first character of prompt value
   const capLetter = playerSelection.charAt(0).toUpperCase();
+  // 2- Lowercase the rest of the prompt value characters exept for the firt one
   const remainLetters = playerSelection.slice(1).toLowerCase();
+  // 3- Concatenate the 2 parts together
   const result = capLetter + remainLetters;
+
+  // Game Logic
   const cpu = getComputerChoice();
 
   if (result === cpu) {
@@ -23,12 +34,15 @@ function playGround() {
   }
 }
 
+// Create prompt asking user for his choice against cpu
 var playerSelection = prompt("Rock, paper or scissors?");
 
+// Declare function game to call the function playGround to repeat the game 5 times
 function game() {
   for (let i = 0; i < 5; i++) {
     playGround();
   }
 }
 
+// Calling the function
 game();
